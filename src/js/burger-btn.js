15b073ea6fbabefa747f675041ec250cb449b234
Kuznetsov Yegor <kuznetsovyegor25@gmail.com>
2022-06-27 05:18:39 +0300

    finished opening/closing side-bar logic

--- src/js/burger-btn.js
+++ src/js/burger-btn.js
@@ -1,22 +1,34 @@
-{
+;(function () {
   const burgerBtn = document.querySelector('.hamburger-btn')
   const closeburgerBtn = document.querySelector('.close-menu-btn')
   const sidebar = document.querySelector('.side-bar')
   const bodycontent = document.querySelector('.body-content')
-  const maincontent = document.querySelector('.main-content')
+  // const maincontent = document.querySelector('.main-content')
   const pagecontent = document.querySelector('.page-content')
 
   console.log(pagecontent.computedStyleMap())
 
-  burgerBtn.addEventListener('click', function () {
-    sidebar.classList.add('side-bar--opened')
-    pagecontent.setAttribute('style', 'opacity: 0.05')
-    bodycontent.setAttribute('style', 'max-height: 900px; overflow: hidden')
-  })
-
-  closeburgerBtn.addEventListener('click', function () {
+  function closeSidebar() {
     sidebar.classList.remove('side-bar--opened')
     pagecontent.removeAttribute('style')
     bodycontent.removeAttribute('style')
+  }
+
+  function openSidebar() {
+    sidebar.classList.add('side-bar--opened')
+    pagecontent.setAttribute('style', 'opacity: 0.05;')
+    bodycontent.setAttribute('style', 'max-height: 900px; overflow: hidden;')
+  }
+
+  burgerBtn.addEventListener('click', function () {
+    openSidebar()
+
+    pagecontent.addEventListener(
+      'click',
+      closeSidebar,
+      sidebar.classList.contains('side-bar--opened')
+    )
+
+    closeburgerBtn.addEventListener('click', closeSidebar)
   })
-}
+})()
